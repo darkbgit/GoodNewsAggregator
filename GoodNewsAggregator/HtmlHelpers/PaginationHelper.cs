78542e0fb9289335e1fdef4d7aeba5cf257bef16
darkbgit <darkb@mail.ru>
2021-05-20 23:21:35 +0300

    5/20/2021

--- GoodNewsAggregator/HtmlHelpers/PaginationHelper.cs
+++ GoodNewsAggregator/HtmlHelpers/PaginationHelper.cs
@@ -13,9 +13,15 @@ namespace GoodNewsAggregator.HtmlHelpers
 {
     public static class PaginationHelper
     {
+        private const int PAGES_FROM_CURRENT_PAGE = 3;
+        private const int NUMBER_OF_FIRST_PAGES = 2;
+        private const int NUMBER_OF_LAST_PAGES = 2;
+
         public static HtmlString PageLinks(this IHtmlHelper html,
             PageInfo pageInfo, Func<int, string> pageUrl)
         {
+            
+
             var nav = new TagBuilder("nav");
             nav.MergeAttribute("aria-label", "Page navigation");
 
@@ -27,13 +33,16 @@ namespace GoodNewsAggregator.HtmlHelpers
             var isLastPage = pageInfo.PageNumber == pageInfo.TotalPages;
 
             var previousPage = isFirstPage ? 1 : pageInfo.PageNumber - 1;
+            var nextPage = isLastPage ? pageInfo.TotalPages : pageInfo.PageNumber + 1;
 
             ul.InnerHtml.AppendHtml(GetHtmlButtonCode(previousPage,
                 pageUrl(previousPage),
                 "Previous",
                 isFirstPage));
 
-            if (pageInfo.TotalPages <= 11)
+            if (pageInfo.TotalPages <= NUMBER_OF_FIRST_PAGES + 
+                                        PAGES_FROM_CURRENT_PAGE * 2 +
+                                        NUMBER_OF_LAST_PAGES + 1)
             {
                 for (int i = 0; i < pageInfo.TotalPages; i++)
                 {
@@ -45,19 +54,37 @@ namespace GoodNewsAggregator.HtmlHelpers
             }
             else
             {
+                bool leftDots = false;
+                bool rightDots = false;
                 for (int i = 0; i < pageInfo.TotalPages; i++)
                 {
+                    
+                    if (i > 1 && pageInfo.PageNumber - PAGES_FROM_CURRENT_PAGE > 2 && !leftDots && pageInfo.PageNumber != NUMBER_OF_FIRST_PAGES + PAGES_FROM_CURRENT_PAGE + 1)
+                    {
+                        ul.InnerHtml.AppendHtml(GetHtmlButtonCode(0,
+                            pageUrl(0),
+                            "...",
+                            true));
+                        leftDots = true;
+                        i = pageInfo.PageNumber - PAGES_FROM_CURRENT_PAGE - 1;
+                    }
+
+                    if ((leftDots||pageInfo.PageNumber < 7) && !rightDots && i > pageInfo.PageNumber + PAGES_FROM_CURRENT_PAGE - 1 && pageInfo.PageNumber != pageInfo.TotalPages - (PAGES_FROM_CURRENT_PAGE + NUMBER_OF_LAST_PAGES + 1))
+                    {
+                        ul.InnerHtml.AppendHtml(GetHtmlButtonCode(0,
+                            pageUrl(0),
+                            "...",
+                            true));
+                        rightDots = true;
+                        i = pageInfo.TotalPages - 2;
+                    }
                     ul.InnerHtml.AppendHtml(GetHtmlButtonCode(i + 1,
                         pageUrl(i + 1),
                         (i + 1).ToString(),
                         i + 1 == pageInfo.PageNumber));
-
-
                 }
             }
-
-            var nextPage = isLastPage ? pageInfo.TotalPages : pageInfo.PageNumber + 1;
-
+        
             ul.InnerHtml.AppendHtml(GetHtmlButtonCode(nextPage,
                 pageUrl(nextPage),
                 "Next",
